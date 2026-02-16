e8ebb299ea7c7db2403d0ae9b154d004f153aca5
arman115data <mdarmaanmunshi@gmail.com>
2026-02-16 20:56:08 +0600

    learn all about selecting elements in dom

---
+++ index.js
@@ -0,0 +1,31 @@
+// let ID1 = document.getElementById("ID1");
+
+// ID1.innerHTML = "Hi Programmer";
+
+//------------------------------------------------------
+
+// let className1 = document.getElementsByClassName("classname1")[3];
+
+// className1.innerHTML = "follow me programmer";
+
+//---------------------------------------------------------------
+
+// let Heading5 = document.getElementsByName("Heading5")[0];
+
+// Heading5.innerHTML = "heading 5"
+
+//----------------------------------------------------------------
+
+// let H1 = document.getElementsByTagName("h1")[1];
+
+// H1.innerHTML = "Heading One is Change";
+
+//--------------------------------------------------------------
+
+// document.querySelector("#ID1");
+
+// document.querySelector(".classname1");
+
+// -------------------------------------------------------------------
+
+document.querySelectorAll("h1")[1];
